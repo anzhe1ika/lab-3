0000a1bc6b041745ee3ffcb8c7ffcc9cd4e5f55e
anzhe1ika <162430723+anzhe1ika@users.noreply.github.com>
2024-03-26 22:18:45 +0200

    Add files via upload

---
+++ Lab3(20.03)/Number.cs
@@ -0,0 +1,90 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3_20._03_
+{
+    internal class Number
+    {
+        public double Num1 { get; set; }
+        public double Num2 { get; set; }
+        /// <summary>
+        /// Default
+        /// </summary>
+        public Number()
+        {
+            Num1 = 0;
+            Num2 = 0;
+        }
+        /// <summary>
+        /// With input parameters
+        /// </summary>
+        /// <param name="n1"></param>
+        /// <param name="n2"></param>
+        public Number(double num1, double num2)
+        {
+            Num1 = num1;
+            Num2 = num2;
+        }
+        public double Sum()
+        {
+            return Num1 + Num2;
+        }
+        public double Difference()
+        { 
+            return Num1 - Num2;
+        }
+        public double Product()
+        {
+            return Num1 * Num2;
+        }
+        public double Division()
+        {
+            if (Num2 != 0)
+            {
+                return (Num1 / Num2);
+            }
+            else
+            {
+                Console.WriteLine("Cannot divide by zero.");
+                return 0;
+            }
+        }
+        public double Remainder()
+        {
+            return Num1 % Num2;
+        }
+        public bool Compare(Number otherNumber)
+        {
+            return Sum() == otherNumber.Sum();
+        }
+
+        public void SaveToJson(string filename)
+        {
+            string json = JsonConvert.SerializeObject(this);
+            File.WriteAllText(filename, json);
+            Console.WriteLine("Object saved to JSON file: " + filename);
+        }
+
+        public static Number LoadFromJson(string filename)
+        {
+            if (!File.Exists(filename))
+            {
+                Console.WriteLine("JSON file does not exist.");
+                return null;
+            }
+
+            string json = File.ReadAllText(filename);
+            Number number = JsonConvert.DeserializeObject<Number>(json);
+            Console.WriteLine("Object loaded from JSON file: " + filename);
+            return number;
+        }
+        ~Number()
+        {
+            Console.WriteLine("The object has been deleted");
+        }
+    }
+}
---
+++ Lab3(20.03)/Program.cs
@@ -0,0 +1,27 @@
+﻿using Lab3_20._03_;
+using Newtonsoft.Json;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Number number1 = new Number(9.2, 78.25);
+
+        number1.SaveToJson("number.json");
+
+        Number number2 = Number.LoadFromJson("number.json");
+
+        Console.WriteLine("The sum of two variables: " + number2.Sum());
+        Console.WriteLine("Difference of two variables: " + number2.Difference());
+        Console.WriteLine("The product of two variables: " + number2.Product());
+        Console.WriteLine("Division of two variables: " + number2.Division());
+        Console.WriteLine("Remainder of dividing two variables: " + number2.Remainder());
+
+        Console.WriteLine();
+
+        Console.WriteLine("Comparing the sum of two pairs of variables: " + number1.Compare(number2));
+
+
+        Console.ReadKey();
+    }
+}
